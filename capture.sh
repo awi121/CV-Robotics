b6682f429199e06fb63492c8e2eb4a50fa1b67ce
Peter Pak <ppak10@gmail.com>
2022-11-14 07:41:19 -0500

    Adds reconstruction examples for pyrealsense2.

---
+++ capture.sh
@@ -0,0 +1,2 @@
+# python venv/lib/python3.8/site-packages/open3d/examples/reconstruction_system/sensors/realsense_recorder.py --output_folder dataset --record_imgs 
+python realsense_recorder.py --output_folder dataset --record_imgs 
---
+++ reconstruction.sh
@@ -0,0 +1,1 @@
+python venv/lib/python3.8/site-packages/open3d/examples/reconstruction_system/run_system.py --config realsense.json --default_dataset dataset --make --register --refine --integrate
